fbcfca897e6fdac3f3caf0cdeaf5a7aa7fdd3c26
IanRic32 <al2183041529@azc.uam.mx>
2024-01-09 21:26:36 -0600

    Sinteizacion del codigo y legibilidad, anexion de tabla coreos

--- Contraseñas1.py
+++ Contraseñas1.py
@@ -1,40 +1,32 @@
-# -*- coding: utf-8 -*-
+
 import sqlite3
 import random
 
 listap = ['in', 'Car', 'Per', 'futUr', 'iv', 'Co', '1nd3x', '@', 'C@H', 'l@m', 'c1', 'l', 'D3B', '4r@N', 'N@n', '1x']
-contrase = []
-r = input("Ingresa el correo electronico:\t")
+correo_electronico = input("Ingresa el correo electronico:\t")
 
-for i in range(1, 5):
-    lista = random.choice(listap)
-    contrase.append(lista)
-
-cad = "ñ".join(contrase)
-contrasena = [str(cad)]
-
+contrasena = [str("ñ".join([random.choice(listap) for _ in range(1, 5)]))]
 conexion = sqlite3.connect('Contraseñas.db')
 cursor = conexion.cursor()
 
 cursor.execute('''
                CREATE TABLE IF NOT EXISTS Contraseñas
-               (Num_Contraseña INTEGER PRIMARY KEY AUTOINCREMENT,
-               Contraseña TEXT)
+               (Contraseña TEXT)
                ''')
 
-cursor.execute('SELECT * FROM Contraseñas WHERE Contraseña=?', [str(cad)])
+cursor.execute('SELECT * FROM Contraseñas WHERE Contraseña=?', contrasena)
 result = cursor.fetchone()
 
 if result:
     print("Esta contraseña ya la usaste\nVuelve a intentarlo")
     conexion.close()
 else:
-    cursor.executemany('INSERT INTO Contraseñas(Num_Contraseña,Contraseña) VALUES(NULL,?)', [contrasena])
-    print("Tu nueva contraseña es:\t", cad, "\nSe guardo en el correo:\t%s" % r, "\nRevisa en la Tabla llamada Correos")
+    cursor.execute('INSERT INTO Contraseñas(Contraseña) VALUES(?)', contrasena)
+    print("Tu nueva contraseña es:\t", contrasena, "\nSe guardo en el correo:\t%s" % correo_electronico, "\nRevisa en la Tabla llamada Correos")
 
     cursor.execute('''
-                     CREATE TABLE IF NOT EXISTS Correos (Num_us INTEGER PRIMARY KEY AUTOINCREMENT,Correo TEXT)                    
+                     CREATE TABLE IF NOT EXISTS Correos (Correo TEXT)                    
                       ''')
-    cursor.executemany('INSERT INTO Correos(Num_us,Correo) VALUES(NULL,?)', [([r])])
+    cursor.execute('INSERT INTO Correos(Correo) VALUES(?)', ([correo_electronico]))
     conexion.commit()
     conexion.close()
